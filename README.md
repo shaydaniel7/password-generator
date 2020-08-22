ee20052f20953ec208eea189431befcb50556569
shay_daniel <shay@isom.org>
2020-08-21 19:45:39 -0700

    readme updated

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pwgenerator
+Random Password Generator
+
+Purpose - 
+A website that generates a random secure password for the user. Includes the following:
+
+1. Option for user to pick number of characters
+2. Option to include special characters 
+3. Option to include uppercase letters
+4. Option to include lowercase letters
+5. Option to choose the length of the password
+6. Generator will generate as many passwords as user would like to have.
+
+
+Built With
+HTML
+CSS
+JavaScript
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var passLength = prompt("How many characters should your password have? Choose b
 
 //this makes sure user follows pw length directions
 while (passLength < 8 || passLength > 128) {
-    passLength = prompt("Password length should be 8-128 characters. Try again.")
+    passLength = prompt("Password length must be 8-128 characters. Try again.")
 }
 
 //function to write pw
@@ -13,10 +13,14 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
 }
+
 //prompts to user to choose pw components
 var confirmSpecialCharacter = confirm("Do you want special characters in your password?");
+
 var confirmUpperCase = confirm("Do you want uppercase letters in your password?");
+
 var confirmLowerCase = confirm("Do you want lowercase letters in your password?");
+
 var confirmNumber = confirm("How about numbers, want some numbers?");
 
 alert("Click on 'Generate Password' to get your new password.  Click again for another one.");
@@ -41,8 +45,9 @@ function generatePassword() {
         newPw += number
     }
     //for loop to match choices of characters to the choice of password length to give final output
-    for (var i = 0; i < userChoiceLength; i++) {
+    for (let i = 0; i < userChoiceLength; i++) {
         output += newPw.charAt(Math.floor(Math.random() * newPw.length));
+
     }
     return output;
 }
@@ -50,10 +55,12 @@ function generatePassword() {
 //adding event listener to the generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 //variable arrays
-var specialChar = "!%&,*-./<>?~";
+var specialChar = "!%&,*-./<>?~"; 11
 var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWYYZ";
 var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var number = "0123456789";
+
 
 
